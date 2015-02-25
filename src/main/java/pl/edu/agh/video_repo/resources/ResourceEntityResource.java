a3b651020abeee7c7aca3d88c37a5c182251f2ee
Marcin <kds@o2.pl>
2015-02-25 22:35:26 +0100

    Added fileName field to Resource Added downloading resource service

--- src/main/java/pl/edu/agh/video_repo/resources/ResourceEntityResource.java
+++ src/main/java/pl/edu/agh/video_repo/resources/ResourceEntityResource.java
@@ -4,16 +4,15 @@ import com.google.common.io.ByteStreams;
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
 import io.dropwizard.hibernate.UnitOfWork;
-import java.io.IOException;
-import java.io.InputStream;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import org.hibernate.HibernateException;
 import pl.edu.agh.video_repo.dao.ResourceDAO;
 import pl.edu.agh.video_repo.model.Resource;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Path("/resource")
 public class ResourceEntityResource {
@@ -37,17 +36,30 @@ public class ResourceEntityResource {
             @FormDataParam("file") FormDataContentDisposition fileDetail) {
 
         long id;
-        
+
         try {
             Resource resource = new Resource(ByteStreams.toByteArray(uploadedInputStream));
+            resource.setFileName(fileDetail.getFileName());
             id = dao.create(resource);
-        } catch(IOException ex) {
+        } catch (IOException ex) {
             return Response.status(500).entity("Failed converting Resource to byte array").build();
-        } catch(HibernateException ex) {
+        } catch (HibernateException ex) {
             return Response.status(500).entity("Failed persiting Resource entity").build();
         }
-        
+
         return Response.status(200).entity(Long.toString(id)).build();
     }
 
+    @GET
+    @UnitOfWork
+    @Path("/download/{id}")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response getResource(@PathParam("id") long id) throws Exception {
+        Resource resource = dao.findById(id);
+        byte[] buffer2 = resource.getContent();
+        String fileName = resource.getFileName();
+        return Response.ok(buffer2, MediaType.APPLICATION_OCTET_STREAM)
+                .header("content-disposition", "attachment; filename = " + fileName)
+                .build();
+    }
 }
