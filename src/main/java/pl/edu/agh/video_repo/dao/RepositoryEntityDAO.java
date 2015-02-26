e0dc9175963b1e659ad95b370bbcecd0490f9267
piotrek <pzdzisiu@gmail.com>
2015-02-26 19:33:07 +0100

    binary relation create update delete

--- src/main/java/pl/edu/agh/video_repo/dao/RepositoryEntityDAO.java
+++ src/main/java/pl/edu/agh/video_repo/dao/RepositoryEntityDAO.java
@@ -3,7 +3,6 @@ package pl.edu.agh.video_repo.dao;
 
 import io.dropwizard.hibernate.AbstractDAO;
 import org.hibernate.SessionFactory;
-import pl.edu.agh.video_repo.model.BagElement;
 import pl.edu.agh.video_repo.model.RepositoryEntity;
 
 public class RepositoryEntityDAO extends AbstractDAO<RepositoryEntity>{
